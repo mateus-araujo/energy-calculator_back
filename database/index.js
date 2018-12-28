25d1257b94dd45fd8b91c8d2b0e083984babfa23
mateus-araujo <mateus_1542@hotmail.com>
2018-12-28 10:10:50 -0300

    Add .env to .gitignore && switch to yarn.lock

--- database/index.js
+++ database/index.js
@@ -2,14 +2,14 @@ require('dotenv').config
 const mongoose = require('mongoose')
 
 if (process.env.NODE_ENV === 'development')
-  mongoose.connect(`mongodb://${process.env.DEV_DB_HOST}/${process.env.DEV_DB_NAME}`, 
-  {
-    useCreateIndex: true,
-    useNewUrlParser: true
-  })
+  mongoose.connect(`mongodb://${process.env.DEV_DB_HOST}/${process.env.DEV_DB_NAME}`,
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true
+    })
 else if (process.env.NODE_ENV === 'production')
-  mongoose.connect(`mongodb://${process.env.PROD_DB_USER}:${process.env.PROD_DB_PASSWORD}` +
-    `@${process.env.PROD_DB_HOST}:${process.env.PROD_DB_PORT}/${process.env.PROD_DB_NAME}`,
+  mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}` +
+    `@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
     {
       useCreateIndex: true,
       useNewUrlParser: true
